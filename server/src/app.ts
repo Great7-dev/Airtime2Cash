2182884fc328ddba10d3416df4ec5a887a117845
Abdulsobur-tech <taofeeq.onijo@decadev.dev>
2022-09-12 15:14:08 +0100

    setup

---
+++ server/src/app.ts
@@ -0,0 +1,26 @@
+
+import express from 'express';
+import db from './models/database.config';
+import cors from 'cors';
+
+
+db.sync().then(() => {
+  console.log('connect to db');
+});
+
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+
+
+
+
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`app listening on ${port}`);
+});
+
+export default app;
