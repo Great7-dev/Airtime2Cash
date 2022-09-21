4913910dc08d65d02d79584c40626f5e34a8555e
Abdulsobur-tech <taofeeq.onijo@decadev.dev>
2022-09-21 23:38:21 +0100

    Resolve landingpage

--- client/src/pages/Landingpage.jsx
+++ client/src/pages/Landingpage.jsx
@@ -6,7 +6,7 @@ import Section2 from '../components/Section-2/Section-2'
 import Text from '../components/text/text'
 import Slide from '../components/slide/slide'
 import Footer from '../components/footer/footer'
-import Navbar from "../components/NavBar/Navbar"
+import Navbar from "../components/NavBar/NavBar"
 // localStorage.setItem('userName', 'Michael')
 
 const Landingpage = () => {
