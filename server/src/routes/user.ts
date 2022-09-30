3bd80d65b090e4f5b9bd5cc55c98ed3727c2c627
Abdulsobur-tech <taofeeq.onijo@decadev.dev>
2022-09-30 16:39:55 +0100

    new commit

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,21 +1,19 @@
 import express from 'express';
 const router = express.Router()
-// import { LoginUser, RegisterUser, Updateprofile, verifyUser } from '../controller/users'
 import { auth } from '../middleware/auth';
 import { changePassword, forgotPassword, LoginUser, RegisterUser, Updateprofile, verifyUser, getUser } from '../controller/users'
 
-router.get("/verify/:token", async(req, res) => {
-const token = req.params.token;
-const response = await verifyUser(token);
-//res.json(response);
-res.redirect("http://localhost:3000/login")
+router.get("/verify/:token", async (req, res) => {
+    const token = req.params.token;
+    const response = await verifyUser(token);
+    //res.json(response);
+    res.redirect("http://localhost:3000/login")
 })
-router.patch('/update/:id',auth, Updateprofile)
 
-
+router.patch('/update/:id', auth, Updateprofile)
 router.post('/create', RegisterUser);
-router.get('/getuser/:id',getUser)
+router.get('/getuser/:id', getUser)
 router.post('/login', LoginUser);
-router.post('/forgotpassword', forgotPassword )
+router.post('/forgotpassword', forgotPassword)
 router.patch('/change-password/:id', changePassword);
 export default router;
